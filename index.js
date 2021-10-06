a76770218fb2d9ca521353461b11e0b3e81affec
Justin_supertree <justin1688@supertree.co>
2021-10-06 17:52:23 +0900

    10/06

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
-const port = 5000;
 // const bodyParser = require("body-parser");
 const { User } = require("./models/User");
+const cookieParser = require("cookie-parser");
 const config = require("./config/key");
+const { auth } = require("./middleware/auth");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 const mongoose = require("mongoose");
+const { request } = require("express");
 mongoose
   .connect(config.mongoURI, {
     // 아래 입력코드는 6.0버전이상으로 더이상 지원안한단다(자체 포함)...
@@ -17,8 +19,13 @@ mongoose
   })
   .then(() => console.log("mongoDB is connect..."))
   .catch((err) => console.log(err));
-app.get("/", (req, res) => res.send("Hello World! 잘해봅시다~"));
+app.get("/", (req, res) => res.send("몽고디비 연결"));
 
+app.get("/api/demodata", (req, res) => {
+  res.send("반갑습네다 동무");
+});
+
+// app.use(cookieParser());
 app.post("/register", (req, res) => {
   const user = new User(req.body);
   user.save((err, userInfo) => {
@@ -29,6 +36,41 @@ app.post("/register", (req, res) => {
   });
 });
 
+app.post("/api/users/login", (req, res) => {
+  // DB에서 요청된 이메일을 찾는다.
+  User.findOne({ email: request.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다.",
+      });
+    }
+    // 비밀번호 매칭확인
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch) return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다." });
+    });
+    // password가 맞다면 토큰 전송
+    user.generateToken((err, user) => {
+      if (err) return res.status(400).send(err);
+      res.cookie("x_auth", user.token).status(200).json({ loginSuccess: true, userId: user._id });
+    });
+  });
+});
+
+// 여기까지 통과했다는건 auth가 true
+app.get("/api/users/auth", auth, (req, res) => {
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmen: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image,
+  });
+});
+
+const port = 5000;
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
--- models/User.js
+++ models/User.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
-
-const useSchema = mongoose.Schema({
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const jwt = require("jsonwebtoken");
+const userSchema = mongoose.Schema({
   name: {
     type: String,
     maxlength: 50,
@@ -10,6 +12,7 @@ const useSchema = mongoose.Schema({
     maxlength: 50,
     trim: true,
   },
+  password: { type: String, minlength: 5 },
   wallet: {
     type: String,
     maxlength: 100,
@@ -30,5 +33,45 @@ const useSchema = mongoose.Schema({
     type: Number,
   },
 });
-const User = mongoose.model("User", useSchema);
+
+userSchema.pre("save", function (next) {
+  var user = this;
+  if (user.isModified("password")) {
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
+
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+  //plainPassword 123456789
+  bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+    if (err) return cb(err), cb(null, isMatch);
+  });
+};
+
+//jsonwebtoken을 이용한 토큰 생성
+userSchema.methods.generateToken = function (cb) {
+  var user = this;
+  var token = jwt.sign(user._id.toHexString(), "secretToken");
+  user.token = token;
+  user.save(function (err, user) {
+    if (err) return cb(err);
+    cb(null, user);
+  });
+};
+
+userSchema.methods.findByToken = function (token, cb) {
+  var user = this;
+  jwt.verify(token);
+};
+
+const User = mongoose.model("User", userSchema);
 module.exports = { User };
